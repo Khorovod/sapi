8d88af361d7179195396b79274251b6dba43185b
thetwo_d <thetwo_d@mail.ru>
2021-05-16 00:21:07 +0400

    update dto is a create dto

--- simpleAPI/DataTransferOblects/CommandUpdateDto.cs
+++ simpleAPI/DataTransferOblects/CommandUpdateDto.cs
@@ -3,17 +3,8 @@ using System.ComponentModel.DataAnnotations;
 
 namespace SimpleAPI.DataTransferObjects
 {
-    //полностью дублирует модель для создания, как исправить?
-    public class CommandUpdateDto
+    public class CommandUpdateDto : CommandCreateDto
     {
-        [Required]
-        [MaxLength(123)]
-        public string HowTo { get; set; }
-
-        [Required]
-        public string Line { get; set; }
-
-        public string Platform { get; set; }
-
+        public CommandUpdateDto() : base() {}
     }
 }
